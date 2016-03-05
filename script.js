52bd08a461bb2d11d337f790d65c29cbca09aa8b
Rakesh Das <rakeshdaskowshik@gmail.com>
2016-03-04 22:31:00 -0600

    Implementing JS....

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+var wallThickness,ovenLength,ovenWidth,ovenHeight,windowLength, windowWidth, thermalConductivity, efficiencyValue, solarFlux;
+
+function getValues(){
+  wallThickness = document.getElementById("wallThickness").value;
+  ovenLength = document.getElementById("ovenLength").value;
+  ovenWidth = document.getElementById("ovenWidth").value;
+  ovenHeight = document.getElementById("ovenHeight").value;
+  windowLength = document.getElementById("windowLength").value;
+  windowWidth = document.getElementById("windowWidth").value;
+  thermalConductivity = document.getElementById("thermalConductivity").value;
+  efficiencyValue = document.getElementById("efficiencyValue").value;
+  solarFlux = document.getElementById("solarFlux").value;
+  
+}
+function calculate(){
+
+}
